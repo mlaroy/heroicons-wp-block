a4eb4c186cb195ce1cbb385bb8b236fca308c2e7
Michael LaRoy <mike.laroy@gmail.com>
2022-11-24 15:42:24 -0800

    add color and alignment supports

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e0ded429f47306d8ee2c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '42cf489afcdfc4e1c2cf');
