ab86c32b3e721463b6909620491d7bd8fb4ede1d
Michael LaRoy <mike.laroy@gmail.com>
2022-11-24 14:43:45 -0800

    run new build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b6964250e6d4ba6de48c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b487b52ae064090d32e1');
