d26813c7091a586610335bcd8bb36062022cc412
Michael LaRoy <mike.laroy@gmail.com>
2022-11-24 15:43:18 -0800

    new build with color/alignment support

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '42cf489afcdfc4e1c2cf');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3a7419df4baefb724e9e');
